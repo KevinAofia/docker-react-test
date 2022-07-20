924fe011f47488c28d8e3c0f3741cb153e428c73
KevinAofia <54653039+KevinAofia@users.noreply.github.com>
2022-07-20 09:49:23 -0600

    pushing blank react project

--- docker-react/src/App.js
+++ docker-react/src/App.js
@@ -7,8 +7,10 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <h1>Way to go Stormer! Now go be great!</h1>
+          <h1>Way to go Stormer!</h1>
+          <h1>Now go be great!</h1>
         </p>
+        <img src={logo} className="App-logo" alt="logo" />
       </header>
     </div>
   );
